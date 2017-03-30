1ea5306bc64b80aa4b715ac582634c146f4ae460
Filin Pavel <pfx@mail.ru>
2017-03-30 20:46:39 +0700

    launched gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var gulp=require('gulp');
 var watermark = require('gulp-watermark');
 var imagemin = require('gulp-imagemin');
+var imageResize = require('gulp-image-resize');
  gulp.task('default',function(){
-gulp.src('src/*')
+gulp.src('images/price/*')
 	.pipe(imagemin())
-    	.pipe(watermark({
-        image: 'test/watermark.png',
+    	
+	.pipe(imageResize({
+	width : 800,
+      	height : 600,
+      	crop : true,
+      	upscale : false
+}))
+	.pipe(watermark({
+        image: 'images/watermark.png',
         gravity: 'Center' }))
-    	.pipe(gulp.dest('dist'))
+
+    	.pipe(gulp.dest('images/dist'))
 });
